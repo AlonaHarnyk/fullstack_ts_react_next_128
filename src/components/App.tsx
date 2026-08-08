e499e4d362b5de876a9d3396d8c63219677f2d7a
Maria <alyonagarnyk@gmail.com>
2026-08-08 07:48:12 +0300

    08-react-formik

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,6 +4,15 @@ export default function App() {
   return (
     <>
       <OrderForm />
+      {/* <Parent>
+        {(value) => <div>Test: {value}</div>}
+      </Parent> */}
+      {/* <Parent>{(value) => <p>{value}</p>}</Parent> */}
     </>
   );
 }
+
+// function Parent({ children }) {
+//   const value = "book";
+//   return <section>{children(value)}</section>;
+// }
